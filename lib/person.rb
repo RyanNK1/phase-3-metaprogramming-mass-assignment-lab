3b6958e2a6d322819af2569b293c0276480c7d70
ryan <ryan.kimani0@gmail.com>
2023-05-29 03:33:27 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  # your code here
+# Target items
+# :name, :birthday, :hair_color, :eye_color, :height,
+# :weight, :handed, :complexion, :t_shirt_size,
+# :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(hash)
+    hash.each do |item, value|
+      self.class.attr_accessor(item)
+      self.send("#{item}=", value)
+    end
+  end
 end
